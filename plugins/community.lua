dc4705b7884aadc3f2728833d2dda870be6eece3
s.gavshin <s.gavshin@tinkoff.ru>
2023-08-30 22:35:45 +0700

    add plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,14 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.motion.nvim-spider" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-yank" },
+  -- { import = "astrocommunity.split-and-window.minimap-vim" },
+  { import = "astrocommunity.editing-support.true-zen-nvim" },
+  -- { import = "astrocommunity.editing-support.multicursors-nvim" },
 }
